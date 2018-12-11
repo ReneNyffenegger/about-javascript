2063198f6e05318804e036ca13ea4058cbc1b7f5
René Nyffenegger <mail@renenyffenegger.ch>
2018-12-11 04:14:01 +0100

    + loop.js

---
+++ statements/for/loop.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env js52
+
+var i;
+for (i=1; i<=10; i++) {
+  print(i + ' squared is ' + i*i);
+}
+// 1 squared is 1
+// 2 squared is 4
+// 3 squared is 9
+// 4 squared is 16
+// 5 squared is 25
+// 6 squared is 36
+// 7 squared is 49
+// 8 squared is 64
+// 9 squared is 81
+// 10 squared is 100
