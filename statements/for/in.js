ff68ae0b320633384efa9efff80260a3a308501b
René Nyffenegger <mail@renenyffenegger.ch>
2018-12-11 04:02:32 +0100

    Add array

--- statements/for/in.js
+++ statements/for/in.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env js52
 
+//
+// Iterating over array
+//
+var a = ['foo', 'bar', 'baz']
+for (i in a) {
+  print(i);
+  // 0
+  // 1
+  // 2
+}
+
+//
+// Iterating over keys in an associative array (hash / dict):
+//
 var d = {x: 'eggs', y: 'why'}
 
 for (k in d) {
   print('d[' + k + '] = ' + d[k])
+  // d[x] = eggs
+  // d[y] = why
 }
